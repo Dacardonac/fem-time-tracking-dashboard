26ec191d28ad5c16d7db148fdc0ba5a2c51199bc
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-03-26 10:56:14 -0500

    feat: implement dynamic card rendering and update styles for improved layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 import Swal from 'sweetalert2';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   let dailyBtn = document.getElementById('dailyBtn');
   let weekBtn = document.getElementById('weeklyBtn');
   let monthBtn = document.getElementById('monthlyBtn');
@@ -9,9 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   async function fetchData() {
     try {
       let response = await fetch('data.json');
-      let data = await response.json();
-      data = data;
-      console.log(data);
+      return await response.json();
     } catch (error) {
       console.error('Error', error);
     }
@@ -29,6 +27,38 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  function printCards(data, timeframe) {
+    const container = document.getElementById('cards-container');
+    container.innerHTML = '';
+
+    data.forEach((item) => {
+      const article = document.createElement('article');
+      article.classList.add('card');
+      article.style.backgroundColor = `var(--${item.title.toLowerCase().replace(/ /g, '-')})`;
+      article.setAttribute('aria-label', `${item.title} tracking card`);
+
+      article.innerHTML = `
+        <header class="card__header">
+          <img class="card__header-icon" src="/src/assets/icons/icon-${item.title.toLowerCase().replace(/ /g, '-')}.svg" alt="${item.title} icon">
+        </header>
+        <div class="card__content">
+          <header class="card__content-header">
+            <h2 class="card__content-title">${item.title}</h2>
+            <button class="card__content-options" aria-label="Options">
+              <img src="./src/assets/icons/icon-ellipsis.svg" alt="Ellipsis icon">
+            </button>
+          </header>
+          <main class="card__content-details">
+            <h3 class="card__content-hours">${item.timeframes[timeframe].current}hrs</h3>
+            <p class="card__content-previous">Last Week - ${item.timeframes[timeframe].previous}hrs</p>
+          </main>
+        </div>
+      `;
+      container.appendChild(article);
+    });
+  }
+
+  data = await fetchData();
   activeButton();
-  fetchData();
+  printCards(data, 'weekly');
 });
